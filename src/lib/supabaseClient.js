017e5bf3795935e362857b861e6cd5dad5ba1606
Florencio Reyes <eldonflor@gmail.com>
2025-07-17 11:53:31 -0600

    feat: add dynamic card logic

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 // src/lib/supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ugvcjfomrnpcnhfrbdpo.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVndmNqZm9tcm5wY25oZnJiZHBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3MDA1NjEsImV4cCI6MjA2ODI3NjU2MX0.DAcdV-UIim-8BA54QGjB-ppx31cIRa9YDqol29SffCI'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
